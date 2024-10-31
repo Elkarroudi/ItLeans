9f7f91c42e3792e6c45e1483a496e51b72224aaf
Elkarroudi <amineelkarroudi04@proton.me>
2024-10-31 13:56:33 +0100

    Add Validation TO Subject DTO's

--- src/main/java/com/itLens/surveyApp/models/dtos/subject/SubjectDTO.java
+++ src/main/java/com/itLens/surveyApp/models/dtos/subject/SubjectDTO.java
@@ -1,13 +1,19 @@
 package com.itLens.surveyApp.models.dtos.subject;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public record SubjectDTO(
-    String id,
-    String title
+        @NotBlank( message = "Survey Id Is Required" )
+        String id,
+
+        @NotBlank( message = "Survey Title Is Required" )
+        @Size(max = 250)
+        String title
 ) {
 
 }
