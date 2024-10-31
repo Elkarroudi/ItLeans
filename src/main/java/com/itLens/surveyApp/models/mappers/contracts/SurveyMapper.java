39d7dc81f714f071bd71aa33332bd43ae8548568
Elkarroudi <amineelkarroudi04@proton.me>
2024-10-31 12:04:38 +0100

    Create Survey Mapper

---
+++ src/main/java/com/itLens/surveyApp/models/mappers/contracts/SurveyMapper.java
@@ -0,0 +1,11 @@
+package com.itLens.surveyApp.models.mappers.contracts;
+
+import com.itLens.surveyApp.models.dtos.survey.SurveyDTO;
+import com.itLens.surveyApp.models.dtos.survey.SurveyResponseDTO;
+import com.itLens.surveyApp.models.entities.Survey;
+import com.itLens.surveyApp.models.mappers.IGenericMapper;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SurveyMapper extends IGenericMapper<Survey, SurveyDTO, SurveyResponseDTO> {
+}
