89953b45cb80f2c3d51865e78a267c26fb66c702
Elkarroudi <amineelkarroudi04@proton.me>
2024-10-31 12:19:53 +0100

    Add Validation To Survey DTO's

--- src/main/java/com/itLens/surveyApp/models/dtos/survey/CreateSurveyDTO.java
+++ src/main/java/com/itLens/surveyApp/models/dtos/survey/CreateSurveyDTO.java
@@ -1,13 +1,21 @@
 package com.itLens.surveyApp.models.dtos.survey;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public record CreateSurveyDTO(
+        @NotBlank( message = "Survey Title Is Required" )
+        @Size(max = 250)
         String title,
+
+        @NotBlank( message = "Survey Description Is Required" )
         String description,
+
+        @NotBlank( message = "Survey Owner Id Is Required" )
         String ownerId
 ) {
 
--- src/main/java/com/itLens/surveyApp/models/dtos/survey/SurveyResponseDTO.java
+++ src/main/java/com/itLens/surveyApp/models/dtos/survey/SurveyResponseDTO.java
@@ -2,6 +2,9 @@ package com.itLens.surveyApp.models.dtos.survey;
 
 import com.itLens.surveyApp.models.dtos.owner.OwnerDTO;
 import com.itLens.surveyApp.models.dtos.surveyEdition.SurveyEditionDTO;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,10 +13,19 @@ import java.util.List;
 @Getter
 @Setter
 public record SurveyResponseDTO(
+        @NotBlank( message = "Survey Id Is Required" )
         String id,
+
+        @NotBlank( message = "Survey Title Is Required" )
+        @Size(max = 250)
         String title,
+
+        @NotBlank( message = "Survey Description Is Required" )
         String description,
+
+        @NotNull( message = "Survey Owner Is Required" )
         OwnerDTO owner,
+
         List<SurveyEditionDTO> surveyEditions
 ) {
 
