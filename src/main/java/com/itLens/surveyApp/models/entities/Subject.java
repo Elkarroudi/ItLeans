ec07550ff97edaf12c284e0a78ecdc4a01407915
Elkarroudi <amineelkarroudi04@proton.me>
2024-10-31 09:56:33 +0100

    Create Subject Class

---
+++ src/main/java/com/itLens/surveyApp/models/entities/Subject.java
@@ -0,0 +1,57 @@
+package com.itLens.surveyApp.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "subjects")
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(
+            name = "title",
+            unique = true,
+            nullable = false
+    )
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "surveyEditionId",
+            nullable = false
+    )
+    private SurveyEdition surveyEdition;
+
+    @OneToMany(
+            mappedBy = "subject",
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL
+    )
+    private List<Question> questions;
+
+    @OneToMany(
+            mappedBy = "parentSubject",
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL
+    )
+    private List<Subject> subSubjects;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "parentSubjectId"
+    )
+    private Subject parentSubject;
+
+}
