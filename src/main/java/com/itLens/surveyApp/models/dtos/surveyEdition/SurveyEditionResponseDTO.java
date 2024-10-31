e253095a10faf63b9d69ead865e3dc34fa39f548
Elkarroudi <amineelkarroudi04@proton.me>
2024-10-31 13:54:28 +0100

    Add Validation To Survey Edition

--- src/main/java/com/itLens/surveyApp/models/dtos/surveyEdition/SurveyEditionResponseDTO.java
+++ src/main/java/com/itLens/surveyApp/models/dtos/surveyEdition/SurveyEditionResponseDTO.java
@@ -2,20 +2,37 @@ package com.itLens.surveyApp.models.dtos.surveyEdition;
 
 import com.itLens.surveyApp.models.dtos.subject.SubjectDTO;
 import com.itLens.surveyApp.models.dtos.survey.SurveyDTO;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @Setter
 public record SurveyEditionResponseDTO(
+        @NotBlank( message = "Owner Id Is Required" )
         String id,
-        String creationDate,
-        String startDate,
-        String endDate,
-        String year,
+
+        @NotNull( message = "Creation Date Is Required" )
+        LocalDate creationDate,
+
+        @NotNull( message = "Start Date Is Required" )
+        @FutureOrPresent
+        LocalDate startDate,
+
+        @NotNull( message = "End Date Is Required" )
+        @Future
+        LocalDate endDate,
+
+        @NotNull( message = "Year Is Required" )
+        @Min(2024)
+        int year,
+
+        @NotNull
         SurveyDTO survey,
+
         List<SubjectDTO> subjects
 ) {
 
