d08bea105399088ba1cc2f63776d7867e6725284
Elkarroudi <amineelkarroudi04@proton.me>
2024-10-31 12:14:18 +0100

    Add Validation To Owner DTO's

--- src/main/java/com/itLens/surveyApp/models/dtos/owner/OwnerDTO.java
+++ src/main/java/com/itLens/surveyApp/models/dtos/owner/OwnerDTO.java
@@ -1,12 +1,18 @@
 package com.itLens.surveyApp.models.dtos.owner;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public record OwnerDTO(
+        @NotBlank( message = "Owner Id Is Required" )
         String id,
+
+        @NotBlank( message = "Owner Name Is Required" )
+        @Size(max = 250)
         String name
 ) {
 
--- src/main/java/com/itLens/surveyApp/models/dtos/owner/OwnerResponseDTO.java
+++ src/main/java/com/itLens/surveyApp/models/dtos/owner/OwnerResponseDTO.java
@@ -1,6 +1,8 @@
 package com.itLens.surveyApp.models.dtos.owner;
 
 import com.itLens.surveyApp.models.dtos.survey.SurveyDTO;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,13 @@ import java.util.List;
 @Getter
 @Setter
 public record OwnerResponseDTO(
+        @NotBlank( message = "Owner Id Is Required" )
         String id,
+
+        @NotBlank( message = "Owner Name Is Required" )
+        @Size(max = 250)
         String name,
+
         List<SurveyDTO> surveys
 ) {
 
