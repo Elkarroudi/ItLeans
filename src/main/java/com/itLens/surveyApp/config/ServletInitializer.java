46cb709ba4427c525c41be3b264938134a5a0ca3
Elkarroudi <amineelkarroudi04@proton.me>
2024-10-31 09:31:01 +0100

    Setup Spring Boot Project

---
+++ src/main/java/com/itLens/surveyApp/config/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.itLens.surveyApp.config;
+
+import com.itLens.surveyApp.SurveyAppApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SurveyAppApplication.class);
+	}
+
+}
