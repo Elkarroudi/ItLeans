e93e431120c7ceaa1b43a90e0ac435d0299fe38a
Elkarroudi <amineelkarroudi04@proton.me>
2024-10-31 12:06:18 +0100

    Create Survey Edition Mapper

---
+++ src/main/java/com/itLens/surveyApp/models/mappers/contracts/SurveyEditionMapper.java
@@ -0,0 +1,11 @@
+package com.itLens.surveyApp.models.mappers.contracts;
+
+import com.itLens.surveyApp.models.dtos.surveyEdition.SurveyEditionDTO;
+import com.itLens.surveyApp.models.dtos.surveyEdition.SurveyEditionResponseDTO;
+import com.itLens.surveyApp.models.entities.SurveyEdition;
+import com.itLens.surveyApp.models.mappers.IGenericMapper;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SurveyEditionMapper extends IGenericMapper<SurveyEdition, SurveyEditionDTO, SurveyEditionResponseDTO> {
+}
