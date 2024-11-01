f5397281d73b2e2456f9ca2714ad46ecbc943536
Elkarroudi <amineelkarroudi04@proton.me>
2024-11-01 10:09:02 +0100

    Modify IGeneric Mapper

--- src/main/java/com/itLens/surveyApp/models/mappers/IGenericMapper.java
+++ src/main/java/com/itLens/surveyApp/models/mappers/IGenericMapper.java
@@ -1,5 +1,8 @@
 package com.itLens.surveyApp.models.mappers;
 
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
 public interface IGenericMapper<Entity, EntityDTO, CreateEntityDTO, EntityResponseDTO> {
 
     Entity toEntity(EntityDTO entityDTO);
@@ -9,4 +12,6 @@ public interface IGenericMapper<Entity, EntityDTO, CreateEntityDTO, EntityRespon
     EntityDTO toDtoFromEntity(Entity entity);
     EntityResponseDTO toResponseDtoFromEntity(Entity entity);
 
+    @Mapping(source = "entityDTO.id", target = "id")
+    Entity updateEntityFromDto(EntityDTO entityDTO, @MappingTarget Entity entity);
 }
